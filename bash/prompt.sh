3c3d69ea83415753f2b5d1e012733fa8d6f57f13
Daniel Baldwin <danielcbaldwin@gmail.com>
2012-05-10 11:44:03 -0600

    updates to prompt

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,22 +1,18 @@
-BOLD="\[\033[1m\]"
-RED="\[\e[38;5;1m\]"
-GREEN="\[\e[0;32m\]"
-BLUE="\[\e[38;5;18m\]"
-YELLOW="\e[33m\]"
-ORANGE="\e[38;5;202m\]"
-OFF="\[\033[m\]"
+BOLD="\033[1m"
+RED="\e[38;5;1m"
+GREEN="\e[0;32m"
+BLUE="\e[38;5;18m"
+YELLOW="\e[33m"
+ORANGE="\e[38;5;202m"
+OFF="\033[m"
 
 # setup the prompt with git branch
-function parse_git_dirty {
+function __parse_git_dirty {
  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo " *"
 }
 
-function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
-}
-
-function parse_rvm_info {
-  echo $(~/.rvm/bin/rvm-prompt)
+function __parse_git_branch {
+  echo -n $(__git_ps1 "${GREEN} (%s${RED}$(__parse_git_dirty)${OFF}${GREEN})${OFF}")
 }
 
 function __my_rvm_ruby_version {
@@ -26,6 +22,5 @@ function __my_rvm_ruby_version {
   local full="$version$gemset"
   [ "$full" != "" ] && echo "$full"
 }
-
-export PS1="${BLUE}[\T]${OFF} [ ${ORANGE}\w${OFF}$(__git_ps1 "${GREEN} (%s${RED}$(parse_git_dirty)${OFF}${GREEN})${OFF}") ${YELLOW}(\$(__my_rvm_ruby_version))${OFF} ] \e[38;5;18m\]$ ${OFF} "
+export PS1="${BLUE}[\T]${OFF} [ ${ORANGE}\w${OFF}\$(__parse_git_branch) ${YELLOW}(\$(__my_rvm_ruby_version))${OFF} ] ${BLUE}$ ${OFF} "
 #export PROMPT_COMMAND='PS1=$PS1; echo -ne "\033]0;`hostname -s`:`pwd`\007"'
