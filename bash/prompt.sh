481ee703c6a4ea48b784c0c6cfbfcb8075fa8759
Daniel Baldwin <danielcbaldwin@gmail.com>
2012-05-18 14:17:48 -0600

    added a vim theme and fixed the prompt so that colors are handled better and so that colors dont count against terminal char count

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,10 +1,42 @@
-BOLD="\033[1m"
-RED="\e[38;5;1m"
-GREEN="\e[0;32m"
-BLUE="\e[38;5;18m"
-YELLOW="\e[33m"
-ORANGE="\e[38;5;202m"
-OFF="\033[m"
+#BOLD="\[\033[1m\]"
+#RED="\[\e[38;5;1m\]"
+#GREEN="\[\e[0;32m\]"
+#BLUE="\[\e[38;5;18m\]"
+#YELLOW="\[\e[33m\]"
+#ORANGE="\[\e[38;5;202m\]"
+#OFF="\[\033[m\]"
+
+if tput setaf 1 &> /dev/null; then
+    tput sgr0
+    if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
+      MAGENTA=$(tput setaf 9)
+      YELLOW=$(tput setaf 3)
+      ORANGE=$(tput setaf 172)
+      GREEN=$(tput setaf 2)
+      PURPLE=$(tput setaf 141)
+      BLUE=$(tput setaf 4)
+      WHITE=$(tput setaf 256)
+    else
+      MAGENTA=$(tput setaf 5)
+      YELLOW=$(tput setaf 3)
+      ORANGE=$(tput setaf 4)
+      GREEN=$(tput setaf 2)
+      PURPLE=$(tput setaf 1)
+      BLUE=$(tput setaf 4)
+      WHITE=$(tput setaf 7)
+    fi
+    BOLD=$(tput bold)
+    OFF=$(tput sgr0)
+else
+    MAGENTA="\033[1;31m"
+    ORANGE="\033[1;33m"
+    GREEN="\033[1;32m"
+    PURPLE="\033[1;35m"
+    BLUE="\033[1;34m"
+    WHITE="\033[1;37m"
+    BOLD="\[\033[1m\]"
+    OFF="\033[m"
+fi
 
 # setup the prompt with git branch
 function __parse_git_dirty {
@@ -12,7 +44,7 @@ function __parse_git_dirty {
 }
 
 function __parse_git_branch {
-  echo -n $(__git_ps1 "${GREEN} (%s${RED}$(__parse_git_dirty)${OFF}${GREEN})${OFF}")
+  echo $(__git_ps1 "%s")
 }
 
 function __my_rvm_ruby_version {
@@ -22,5 +54,11 @@ function __my_rvm_ruby_version {
   local full="$version$gemset"
   [ "$full" != "" ] && echo "$full"
 }
-export PS1="${BLUE}[\T]${OFF} [ ${ORANGE}\w${OFF}\$(__parse_git_branch) ${YELLOW}(\$(__my_rvm_ruby_version))${OFF} ] ${BLUE}$ ${OFF} "
+PREAMBLE="\[${GREEN}\][\T]\[${OFF}\]"
+DIRINFO="\[${ORANGE}\]\w\[${OFF}\]"
+GITINFO="\[${GREEN}\]\$([[ -n \$(__parse_git_branch) ]] && echo \"(\")\$(__parse_git_branch)\[${MAGENTA}\]\$([[ -n \$(__parse_git_branch) ]] && echo \"\$(__parse_git_dirty)\")\[${OFF}\]\[${GREEN}\]\$([[ -n \$(__parse_git_branch) ]] && echo \")\")\[${OFF}\]"
+RVMINFO="\[${YELLOW}\](\$(__my_rvm_ruby_version))\[${OFF}\]"
+POSTSCRIPT="\[${BLUE}\]$ \[${OFF}\]"
+PS1="${PREAMBLE} [ ${DIRINFO}${GITINFO} ${RVMINFO} ] ${POSTSCRIPT}"
+export PS1="${PS1}"
 #export PROMPT_COMMAND='PS1=$PS1; echo -ne "\033]0;`hostname -s`:`pwd`\007"'
