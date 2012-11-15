5703d68be7068e0c0a635b4d82d7cb9422387387
Daniel Baldwin <danielcbaldwin@gmail.com>
2012-11-15 13:25:02 -0700

    Updates

--- bash/lib/enviroment.bash
+++ bash/lib/enviroment.bash
@@ -9,5 +9,7 @@ export LANG="en_US"
 export TERM="xterm-256color"
 
 # Make the MySQL Ruby Gem build properly
-#export DYLD_LIBRARY_PATH="/usr/local/mysql/lib:$DYLD_LIBRARY_PATH"
+if [[ "$OSTYPE" =~ "darwin" ]]; then
+  export DYLD_LIBRARY_PATH="/usr/local/mysql/lib:$DYLD_LIBRARY_PATH"
+fi
 
