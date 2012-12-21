1cdd966e62de553df9714ad2bbf15ad5ab276aed
Daniel Baldwin <danielcbaldwin@gmail.com>
2012-12-21 14:05:30 -0700

    Testing Update

--- bash/themes/daniel/daniel.theme.bash
+++ bash/themes/daniel/daniel.theme.bash
@@ -19,8 +19,14 @@ function rvm_version_prompt {
   fi
 }
 
+function ssh_show_host {
+  if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+    echo -e "${cyan}[\h]${reset_color} "
+  fi
+}
+
 function prompt_command() {
-    PS1="${green}[\T]${reset_color} [ ${cyan}\w${reset_color}$(scm_prompt_info)${reset_color}${yellow}$(ruby_version_prompt)${reset_color} ] ${cyan}\$ ${reset_color}"
+  PS1="$(ssh_show_host)${green}[\T]${reset_color} [ ${cyan}\w${reset_color}$(scm_prompt_info)${reset_color}${yellow}$(ruby_version_prompt)${reset_color} ] ${cyan}\$ ${reset_color}"
 }
 
 PROMPT_COMMAND=prompt_command;
