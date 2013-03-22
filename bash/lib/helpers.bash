d7726454b73e14d5b747a2a72be2f16b7d523620
Daniel Baldwin <danielcbaldwin@gmail.com>
2013-03-22 15:09:45 -0600

    Changes

--- bash/lib/helpers.bash
+++ bash/lib/helpers.bash
@@ -31,8 +31,5 @@ function reload_plugins() {
 
 _is_function ()
 {
-    _about 'sets $? to true if parameter is the name of a function'
-    _param '1: name of alleged function'
-    _group 'lib'
     [ -n "$(type -a $1 2>/dev/null | grep 'is a function')" ]
 }
