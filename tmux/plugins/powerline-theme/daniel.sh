32cbddab92db7a6183a4f86504d26b11038cfbcf
Daniel Baldwin <danielcbaldwin@gmail.com>
2013-04-15 16:25:25 -0600

    tmux Updates

--- tmux/plugins/powerline-theme/daniel.sh
+++ tmux/plugins/powerline-theme/daniel.sh
@@ -25,8 +25,8 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		"hostname 33 0" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
+		"lan_ip 24 255" \
+		#"wan_ip 24 255" \
 		#"vcs_branch 29 88" \
 		#"vcs_compare 60 255" \
 		#"vcs_staged 64 255" \
@@ -39,11 +39,11 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"pwd 89 211" \
 		#"mail_count_maildir 9 255" \
-		"now_playing 234 37" \
+		#"now_playing 234 37" \
 		#"cpu 240 136" \
-		"load 237 167" \
+		#"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
+		#"battery 137 127" \
 		#"weather 37 255" \
 		#"xkb_layout 125 117" \
 		"date_day 235 136" \
