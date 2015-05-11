8e8593cc148f8f51eb40505c7eb7fd7563cd9cc1
Jason Shultz <jshultz@users.noreply.github.com>
2015-05-11 10:50:14 -0600

    Update rbenv.bash
    typo.

--- bash/plugins/rbenv.bash
+++ bash/plugins/rbenv.bash
@@ -1,7 +1,7 @@
 if [ -d $HOME/.rbenv ]; then
   export RBENV_ROOT="$HOME/.rbenv"
   export PATH="$HOME/.rbenv/bin:$PATH"
-  if [ -n "$(which rben)" ]; then
+  if [ -n "$(which rbenv)" ]; then
     eval "$(rbenv init -)"
   fi
 fi
