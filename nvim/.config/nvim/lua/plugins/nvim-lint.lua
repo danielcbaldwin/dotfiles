6ff8f7f03f9a2c0440f2e7428f4fd3b3930b5e49
Daniel Baldwin <danielcbaldwin@gmail.com>
2023-03-02 16:04:53 -0700

    nvim updates

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,14 +1,15 @@
 -- ============= nvim-lint ===============
 return {
-  "mfussenegger/nvim-lint",
-  event = {"BufWritePost", "VeryLazy"},
-  config = function()
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-      callback = function()
-        require("lint").try_lint()
-      end,
-    })
-  end,
+  --"mfussenegger/nvim-lint",
+  --event = {"BufWritePost"},
+  --init = function()
+  --  vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  --    callback = function()
+  --      require("lint").try_lint()
+  --    end,
+  --  })
+  --end,
+  --config = function()
+  --end,
 }
 -- ==========================================
-
--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -3,4 +3,20 @@ return {
     "tpope/vim-fugitive",
     cmd = {"Git", "GitBlame", },
   },
+  {
+    "dense-analysis/ale",
+    init = function()
+      vim.g.ale_fix_on_save = 1
+      vim.g.ale_lint_on_enter = 0
+      vim.g.ale_lint_on_insert_leave = 0
+      vim.g.ale_lint_on_filetype_changed = 0
+      vim.g.ale_lint_on_text_changed = 'never'
+      vim.g.ale_disable_lsp = 1
+      vim.g.ale_fixers = {
+        [ '*' ] = { "remove_trailing_lines", "trim_whitespace" },
+        [ 'javascript' ] = 'prettier',
+        [ 'css' ] = 'prettier',
+      }
+    end,
+  }
 }
