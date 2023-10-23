ba54032c9d26b68f5c5899e763382a49e7e011c1
Daniel Baldwin <danielcbaldwin@gmail.com>
2023-10-23 14:11:14 -0600

    node updates

--- bash/.bash/plugins/node.plugin.bash
+++ bash/.bash/plugins/node.plugin.bash
@@ -2,3 +2,7 @@ if [ -d $HOME/.nvm ]; then
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 fi
+
+if [ -d $HOME/.nodeenv ]; then
+  eval "$(nodenv init -)"
+fi
