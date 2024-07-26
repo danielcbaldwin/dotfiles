4e98da28c7e795df3570a50450249a6128e6a0e6
Daniel Baldwin <danielcbaldwin@gmail.com>
2024-07-26 12:34:45 -0600

    updates

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -2,33 +2,45 @@ require "nvchad.autocmds"
 
 local create_autocmd = vim.api.nvim_create_autocmd
 
-create_autocmd({"VimEnter"}, {pattern = "*", callback = function(data)
-  -- buffer is a directory
-  local directory = vim.fn.isdirectory(data.file) == 1
+create_autocmd({ "VimEnter" }, {
+  pattern = "*",
+  callback = function(data)
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
 
-  local args = {}
-  for _,v in pairs(vim.v.argv) do
-    if string.sub(v,1,2) ~= "--" then
-      table.insert(args, v)
+    local args = {}
+    for _, v in pairs(vim.v.argv) do
+      if string.sub(v, 1, 2) ~= "--" then
+        table.insert(args, v)
+      end
     end
-  end
 
-  if not directory then
-    if 1 == #args then
-      vim.cmd('NvimTreeToggle')
+    if not directory then
+      if 1 == #args then
+        vim.cmd "NvimTreeToggle"
+      end
+      return
     end
-    return
-  end
 
-  -- create a new, empty buffer
-  vim.cmd.enew()
+    -- create a new, empty buffer
+    vim.cmd.enew()
 
-  -- wipe the directory buffer
-  vim.cmd.bw(data.buf)
+    -- wipe the directory buffer
+    vim.cmd.bw(data.buf)
 
-  -- change to the directory
-  vim.cmd.cd(data.file)
+    -- change to the directory
+    vim.cmd.cd(data.file)
 
+    vim.cmd "NvimTreeToggle"
+  end,
+})
 
-  vim.cmd('NvimTreeToggle')
-end})
+create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.tf,*.tfvars",
+  command = "setfiletype tf",
+})
+
+create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.as,*.json,*.js",
+  command = "setfiletype javascript",
+})
