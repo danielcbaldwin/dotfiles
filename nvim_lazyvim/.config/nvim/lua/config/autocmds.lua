7bbc3e672b83c9f339d099feb7d86d68a0277083
Daniel Baldwin <danielcbaldwin@gmail.com>
2024-09-28 22:08:16 -0600

    go settings

--- nvim_lazyvim/.config/nvim/lua/config/autocmds.lua
+++ nvim_lazyvim/.config/nvim/lua/config/autocmds.lua
@@ -12,3 +12,8 @@
 --   pattern = "*.as,*.json,*.js",
 --   command = "setfiletype javascript",
 -- })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  command = "setlocal ts=2 sw=2 sts=0 noexpandtab autoindent nolist",
+})
